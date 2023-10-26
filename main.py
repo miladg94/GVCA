63e5b3186f6bae653e056ee28d3664edb8807477
rnilicl <ghanbari.milad.94@gmail.com>
2023-10-26 16:55:26 +0200

    Added weighted DCT

--- main.py
+++ main.py
@@ -6,34 +6,25 @@ import math
 
 @jit(target_backend='cuda', nopython=True)
 def performDCT(Y, block_size, width, height):
-    block = np.zeros((block_size, block_size), dtype=np.uint8)
     blocks = []
     blocks_count = 0
     for a in range(0, height, block_size):
         for b in range(0, width, block_size):
             block = Y[a:a + block_size, b:b + block_size]
-            # FIX PADDING !
-            #for c in range (0, a + block_size - height):
-            #    block[height-a+c,:] = block[height-a-1,:]
-            #for d in range (0, b + block_size - width):
-            #    block[:, width-b+d] = block[:, width-b-1]
             block = block.flatten()
             block = block.astype('int32')
             blocks.append(block)
             blocks_count += 1
     avgs = []
-
     for i in range(blocks_count):
         buffer = dct.partial_butterfly32(blocks[i], 4, 32)
         DCT = dct.partial_butterfly32(buffer, 11, 32)
-        avg = sum(DCT) / len(DCT)
+        WDCT = np.abs(DCT) * dct.weights_dct32 / 256
+        avg = sum(WDCT) / len(WDCT)
         avgs.append(avg)
-    return avgs
+    return sum(avgs)/len(avgs)
 
 
-def cpu(Y, block_size, width, height):
-    averages = performDCT(Y, block_size, width, height)
-    return averages
 @jit(target_backend='cuda', nopython=True)
 def gpu(Y, block_size, width, height):
     averages = performDCT(Y, block_size, width, height)
@@ -41,12 +32,13 @@ def gpu(Y, block_size, width, height):
 
 
 def main():
-    stream = input("Enter path to input file: ")
+    stream = r'C:\Users\rnili\OneDrive\Desktop\VCA-stable\test.yuv'
+    #stream = input("Enter path to input file: ")
     # C:\Users\rnili\OneDrive\Desktop\VCA-stable\test.yuv
     w = 1920
     h = 1080
     block_size = 32
-    frame_count = 120*5
+    frame_count = 600
     frame_size = int(1.5 * w * h)
     totalAverageEnergy = []
     with open(stream, 'rb') as file:
@@ -60,9 +52,9 @@ def main():
             Y = np.frombuffer(frame_data[:width * height], dtype=np.uint8).reshape(height, width)
             Y = Y.copy()
             for hp in range(hpad):
-                Y[h + hp] = Y[h - 1]
+                Y[h + hp, :] = Y[h - 1, :]
             for wp in range(wpad):
-                Y[w + wp] = Y[w - 1]
+                Y[:, w + wp] = Y[:, w - 1]
             start = timer()
             averageEnergy = gpu(Y, block_size, width, height)
             totalAverageEnergy.append(averageEnergy)
