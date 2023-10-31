ef9fe99121c4892f69a5dafd358633ba29e82d57
rnilicl <ghanbari.milad.94@gmail.com>
2023-10-31 17:04:03 +0100

    Temporal complexity added

--- main.py
+++ main.py
@@ -25,55 +25,62 @@ def performDCT(Y, block_size, width, height):
         avg = sumW/len(WDCT)
         avgs.append(avg)
         frameTex += sumW
-    return frameTex // (blocks_count * 90), avgs
+    return frameTex // (blocks_count * 90), avgs, blocks_count
 
 
 @jit(target_backend='cuda', nopython=True)
 def gpu(Y, block_size, width, height):
-    avgEnergy, blocksAVG = performDCT(Y, block_size, width, height)
-    return avgEnergy, blocksAVG
+    avgEnergy, blocksAVG, blkcnt = performDCT(Y, block_size, width, height)
+    return avgEnergy, blocksAVG, blkcnt
 
 
 def main():
-    stream = r'C:\Users\rnili\OneDrive\Desktop\VCA-stable\0001_426x240.yuv'
+    stream = r'C:\Users\rnili\OneDrive\Desktop\VCA-stable\test.yuv'
     #stream = input("Enter path to input file: ")
     # C:\Users\rnili\OneDrive\Desktop\VCA-stable\test.yuv
-    w = 426
-    h = 240
+
+    w = 1920
+    h = 1080
     block_size = 32
-    frame_count = 2
+    height = (math.ceil(h / block_size)) * block_size
+    width = (math.ceil(w / block_size)) * block_size
+    Y = np.zeros((height, width), dtype=np.uint8)
+    hpad = height - h
+    wpad = width - w
+
+    frame_count = 600
     frame_size = int(1.5 * w * h)
-    totalAverageEnergy = []
-    #bAVG1 = np.zeros(frame_count)
-    #bAVG2 = np.zeros(frame_count)
-    #tempComps = np.zeros(frame_count)
+
+    avgsE = []
+    avg1 = np.zeros(frame_count)
+    avg2 = np.zeros(frame_count)
+    tempComps = []
+
     with open(stream, 'rb') as file:
-        go = timer()
+        start = timer()
         for f in range(frame_count):
             frame_data = file.read(frame_size)
             _Y = np.frombuffer(frame_data[:w * h], dtype=np.uint8).reshape(h, w)
-            height = (math.ceil(h / block_size)) * block_size
-            width = (math.ceil(w / block_size)) * block_size
-            hpad = height - h
-            wpad = width - w
-            Y = np.zeros((height, width), dtype=_Y.dtype)
             Y[:h, :w] = _Y
             for hp in range(hpad):
                 Y[h + hp, :] = Y[h - 1, :]
             for wp in range(wpad):
                 Y[:, w + wp] = Y[:, w - 1]
-            start = timer()
-            averageEnergy, blocksAVG = gpu(Y, block_size, width, height)
-            totalAverageEnergy.append(averageEnergy)
-            #bAVG2 = blocksAVG.copy()
-            #tempComp = np.abs(bAVG2 - bAVG1)
-            #tempComp [f-1] = tempComp [f]
-            #bAVG1 = bAVG2.copy()
-            #tempComps.append(tempComp)
-            print(f, totalAverageEnergy[f])
-            #print("GPU Frame Time:", f, timer() - start)
-        print(" GPU Total Time:", timer() - go)
-    return totalAverageEnergy
+
+            Ftime = timer()
+            avgE, blocksAVG, blocks_count = gpu(Y, block_size, width, height)
+            avgsE.append(avgE)
+
+            avg1 = np.resize(avg1, blocks_count)
+            avg2 = blocksAVG.copy()
+            tempComp = np.abs(avg2 - avg1)
+            tempComps.append(tempComp)
+            avg1 = avg2.copy()
+
+            #print(f, avgsE[f])
+            print("GPU Frame Time:", f, timer() - Ftime)
+        print(" GPU Total Time:", timer() - start)
+    return totalAverageEnergy, tempComps
 
 
 if __name__ == "__main__":
